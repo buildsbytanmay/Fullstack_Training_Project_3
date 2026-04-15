c48e423ded6115042c744907162423d2985a5ffb
buildsbytanmay <dushantb123@gmail.com>
2026-04-15 19:44:37 +0530

    Modified deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Fullstack_Training_Project_3/',
   plugins: [
     tailwindcss(),
     react(),
